ef8787dcdc19017bf3973b4f1e099fce8d7a6d6d
Renuzit.V <nvminhduy2002@gmail.com>
2023-12-06 22:11:15 +0700

    updated base repo

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify
+
+from middleware.auth_middleware import token_required
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    """
+    Hello world
+    :return: Hello World message
+    """
+    return 'Hello World!!!'
+
+
+@app.route('/test_auth')
+@token_required
+def test_auth(token):
+    """
+    Test auth
+    does not need to check for authorization. if we get here, it means that the token is valid
+    :param token: returned token from `token_required`
+    :return: authorized message and claims from JWT token
+    """
+
+    return jsonify({
+        "message": "Authorized",
+        "data": token
+    }), 200
 
 
 if __name__ == '__main__':
---
+++ middleware/auth_middleware.py
@@ -0,0 +1,70 @@
+import os
+from functools import wraps
+from jose import jwt
+from flask import request, abort
+import requests
+
+
+def get_jwks(bearer_token):
+    headers = {
+        "Authorization": f"Bearer {bearer_token}"
+    }
+    response = requests.get('https://api.clerk.com/v1/jwks', headers=headers)
+    return response.json()
+
+
+def decode_jwt(token):
+    jwks = get_jwks(os.getenv("CLERK_API_KEY"))
+
+    header = jwt.get_unverified_header(token)
+
+    key = [k for k in jwks['keys'] if k["kid"] == header["kid"]][0]
+
+    decoded = jwt.decode(token, key)
+
+    return decoded
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if "Authorization" in request.headers:
+            token = request.headers["Authorization"].split(" ")[1]
+        if not token:
+            return {
+                       "message": "Authentication Token is missing!",
+                       "data": None,
+                       "error": "Unauthorized"
+                   }, 401
+        try:
+            data = decode_jwt(token)
+        except jwt.ExpiredSignatureError:
+            return {
+                       "message": "Token is expired!",
+                       "data": None,
+                       "error": "Unauthorized"
+                   }, 401
+        except jwt.JWTClaimsError:
+            return {
+                       "message": "Token is invalid!",
+                       "data": None,
+                       "error": "Unauthorized"
+                   }, 401
+        except jwt.JWTError:
+            return {
+                       "message": "Token is invalid!",
+                       "data": None,
+                       "error": "Unauthorized"
+                   }, 401
+        except Exception as e:
+            print("error 500:\n", e)
+            return {
+                       "message": "Something went wrong",
+                       "data": None,
+                       "error": str(e)
+                   }, 500
+
+        return f(data, *args, **kwargs)
+
+    return decorated
