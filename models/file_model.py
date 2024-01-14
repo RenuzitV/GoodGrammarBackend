bf05646d6ec4cb9117aa843d1c77216de0bcc731
ThucT4 <trithuc.t4@gmail.com>
2024-01-14 18:55:56 +0700

    Thuc add created_at to filemodel, add get file and info in file route

--- models/file_model.py
+++ models/file_model.py
@@ -1,8 +1,10 @@
-from mongoengine import Document, StringField, BinaryField
+import datetime
+from mongoengine import Document, StringField, BinaryField, DateTimeField
 
 class FileObject(Document):
     file_name = StringField(required=True)
     file = BinaryField()
+    created_at = DateTimeField(required=True, default=datetime.datetime.now)
 
     def get_file_name(self):
         return self.file_name
@@ -10,3 +12,5 @@ class FileObject(Document):
     def get_file(self):
         return self.file
 
+    def get_create_at(self):
+        return self.created_at
--- services/file_service.py
+++ services/file_service.py
@@ -3,9 +3,6 @@ from models.file_model import FileObject
 
 def save_file(filename="", encodedBinFile=None):
     try:
-        if FileObject.objects(file_name=filename).first():
-            print("File already exist")
-
         newFile = FileObject(file_name=filename, file=encodedBinFile)
         newFile.save()
 
