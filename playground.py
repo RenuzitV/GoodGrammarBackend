b91bf02ac0d2cd9f9f43e6e11a6241342539120e
Duy Nguyen <s3878076@rmit.edu.vn>
2024-01-15 15:46:49 +0700

    checked if clerk user has a name

--- playground.py
+++ playground.py
@@ -1,19 +1,15 @@
-"""
-    this file is purely for testing purposes
-    it is not part of the main project
-"""
-import os
+a = "asd"
+b = None
 
-from dotenv import load_dotenv
-import stripe
-from stripe import Subscription, ListObject
+c = ""
 
-load_dotenv()
+if a is not None:
+    c += a
 
-stripe.api_key = os.getenv('STRIPE_API_KEY')
-customer = stripe.Customer.retrieve(os.getenv("TEST_CUSTOMER_ID"), expand=["subscriptions"])
-# print(customer)
-subscriptions = customer.subscriptions.data
-filtered = filter(lambda subscription: subscription.status == "active", subscriptions)
-active_subscriptions = list(filtered)
-print(active_subscriptions)
+if b is not None:
+    c += " " + b
+
+if c is None or c == "":
+    c = "John Doe"
+
+print(c)
